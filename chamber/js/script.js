a52258e998d892c7dd361dc479fd68787cd445ef
brentsbrooks <brentsbrooks@yahoo.com>
2022-05-23 13:10:01 +0200

    banner

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -19,3 +19,18 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
 datefieldUK.innerHTML = `${fulldateUK}`;
+
+
+let d = new Date().getDay();
+
+const banner = document.getElementById("banner");
+if (d === 1 || d === 2) {
+	banner.style.display = "flex";
+	banner.style.justifyContent = "space-between";
+}
+
+const close1 = document.querySelector("#close");
+close1.style.color = "red" ;
+close1.addEventListener("click", () => {
+	banner.style.display = "none";
+});
